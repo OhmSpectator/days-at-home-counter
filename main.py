c4048bda226b1c421ddc1bbb5ee4d60ce0f289ca
Nikolay Martyanov <ohmspectator@gmail.com>
2022-12-07 17:51:02 +0100

    Fix the escape func usage.

--- main.py
+++ main.py
@@ -25,16 +25,12 @@ calendar_form = """
 
 @app.route("/days-at-home")
 def index():
-    day = request.args.get('day', str(datetime.date.today()))
-    escape(day)
-    interval_start_p = request.args.get('interval-start', '')
-    escape(interval_start_p)
-    interval_end_p = request.args.get('interval-end', '')
-    escape(interval_end_p)
+    day = escape(request.args.get('day', str(datetime.date.today())))
+    interval_start_p = escape(request.args.get('interval-start', ''))
+    interval_end_p = escape(request.args.get('interval-end', ''))
     interval_start = None
     interval_end = None
-    remove = request.args.get('remove', None)
-    escape(remove)
+    remove = escape(request.args.get('remove', None))
     at_home[:] = [interval for interval in at_home if interval.id != remove]
     try:
         interval_start = datetime.date.fromisoformat(interval_start_p)
